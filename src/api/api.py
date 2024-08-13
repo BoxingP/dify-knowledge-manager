8407cb716237aafa3b7b3469524b3b3632bd7383
Boxing <boxingp@gmail.com>
2024-08-13 18:36:35 +0800

    MD-2: modify the use of header parameters in the api.

--- src/api/api.py
+++ src/api/api.py
@@ -2,9 +2,9 @@ import requests
 
 
 class Api(object):
-    def __init__(self, base_url, headers=None):
+    def __init__(self, base_url, secret_header=None):
         self.base_url = base_url
-        self.headers = headers
+        self.secret_header = secret_header
 
     def request(self, method, endpoint, **kwargs):
         url = f'{self.base_url}/{endpoint}'
@@ -16,14 +16,21 @@ class Api(object):
             return None
         return response.json()
 
-    def fetch_data(self, endpoint, params=None):
-        return self.request('GET', endpoint, params=params, headers=self.headers)
+    def _merge_headers(self, headers: dict):
+        return {**self.secret_header, **(headers or {})}
 
-    def post_data(self, endpoint, data=None):
-        return self.request('POST', endpoint, json=data, headers=self.headers)
+    def fetch_data(self, endpoint, headers=None, params=None):
+        headers = self._merge_headers(headers)
+        return self.request('GET', endpoint, params=params, headers=headers)
 
-    def put_data(self, endpoint, data=None):
-        return self.request('PUT', endpoint, json=data, headers=self.headers)
+    def post_data(self, endpoint, headers=None, data=None):
+        headers = self._merge_headers(headers)
+        return self.request('POST', endpoint, json=data, headers=headers)
 
-    def delete_data(self, endpoint):
-        return self.request('DELETE', endpoint, headers=self.headers)
+    def put_data(self, endpoint, headers=None, data=None):
+        headers = self._merge_headers(headers)
+        return self.request('PUT', endpoint, json=data, headers=headers)
+
+    def delete_data(self, endpoint, headers=None):
+        headers = self._merge_headers(headers)
+        return self.request('DELETE', endpoint, headers=headers)
--- src/api/dify_api.py
+++ src/api/dify_api.py
@@ -3,8 +3,7 @@ from src.api.api import Api
 
 class DifyApi(Api):
     def __init__(self, url, secret_key):
-        headers = {'Authorization': f'Bearer {secret_key}'}
-        super(DifyApi, self).__init__(base_url=url, headers=headers)
+        super(DifyApi, self).__init__(base_url=url, secret_header={'Authorization': f'Bearer {secret_key}'})
 
     def get_dataset_id_by_name(self, name) -> str:
         response = self.fetch_data('datasets', params={'page': 1, 'limit': 20})
@@ -33,7 +32,7 @@ class DifyApi(Api):
         return documents
 
     def create_document(self, dataset_id, document_name):
-        self.headers['Content-Type'] = 'application/json'
+        headers = {'Content-Type': 'application/json'}
         data = {
             'name': document_name,
             'text': '',
@@ -42,7 +41,7 @@ class DifyApi(Api):
                 'mode': 'automatic'
             }
         }
-        response = self.post_data(f'datasets/{dataset_id}/document/create_by_text', data=data)
+        response = self.post_data(f'datasets/{dataset_id}/document/create_by_text', headers=headers, data=data)
         return response['document']['id']
 
     def delete_document(self, dataset_id, document_id):
@@ -53,18 +52,18 @@ class DifyApi(Api):
             return f'{document_id} has failed to delete: {e}'
 
     def get_segments_from_document(self, dataset_id, document_id):
-        self.headers['Content-Type'] = 'application/json'
+        headers = {'Content-Type': 'application/json'}
         endpoint = f'datasets/{dataset_id}/documents/{document_id}/segments'
-        response = self.fetch_data(endpoint)
+        response = self.fetch_data(endpoint, headers=headers)
         segments = response['data']
         keys = ['id', 'position', 'document_id', 'content', 'answer', 'keywords']
         return [{key: segment[key] for key in keys} for segment in segments]
 
     def create_segment_in_document(self, dataset_id, document_id, content, answer, keywords):
-        self.headers['Content-Type'] = 'application/json'
+        headers = {'Content-Type': 'application/json'}
         endpoint = f'datasets/{dataset_id}/documents/{document_id}/segments'
         data = {
             'segments': [{'content': content, 'answer': answer, 'keywords': keywords}]
         }
-        response = self.post_data(endpoint, data=data)
+        response = self.post_data(endpoint, headers=headers, data=data)
         return response
